12558a51ab944ee54d38c46977c499f4d595d552
Bijan Rafraf <bijan7@gmail.com>
2026-05-28 23:45:44 +0200

    migrate to nextjs

---
+++ _next/static/A1XysWeJBo1XxibhbOqUW/_buildManifest.js
@@ -0,0 +1,30 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0q_437nlyimqt.js"
+  ],
+  "/404": [
+    "static/chunks/07p53d4u5y614.js"
+  ],
+  "/_error": [
+    "static/chunks/02f-xah3cuzmn.js"
+  ],
+  "/bernoulli-estimation": [
+    "static/chunks/047--8jjuz414.js"
+  ],
+  "/pomodoro": [
+    "static/chunks/0szdqchzztc8b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/bernoulli-estimation",
+    "/pomodoro"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
